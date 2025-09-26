3c49a55a543b09c68816e51950abca55e0e2cce4
samarth-sachin <samarthpatil@yahoo.com>
2025-09-26 15:45:05 +0530

    hatt

--- out/production/coding/ASEP/script.js
+++
@@ -1,7 +0,0 @@
-function callAmbulance() {
-    alert("Calling Ambulance...");
-}
-
-function connectBluetooth() {
-    alert("Connecting to Bluetooth...");
-}
--- out/production/coding/cpp/helloworld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{int i;
-    cout<<"hello world"<<i<<"jai"
-    
-}
